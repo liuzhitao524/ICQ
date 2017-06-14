069c8adef28fa4522e463868ec065f16a7da1f64
tianshuiyue <liuzhitao1990@126.com>
2017-06-14 02:04:21 -0500

    Create SwingConsole.java

---
+++ SwingConsole.java
@@ -0,0 +1,15 @@
+package com.tianshuiyue.TcpIp;
+
+import javax.swing.JFrame;
+
+public class SwingConsole 
+{
+	public static void run(final JFrame f ,final int width,final int height)
+	{
+		f.setTitle("Tcp/Ip");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(width, height);
+		
+		f.setVisible(true);
+	}
+}
